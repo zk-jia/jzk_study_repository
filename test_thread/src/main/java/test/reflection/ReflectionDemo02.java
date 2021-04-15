617e005143fa6af8e6f8ac29c22b352106021647
jiazk <ZKjia721>
2021-04-15 17:16:56 +0800

    关于类加载

--- test_thread/src/main/java/test/reflection/ReflectionDemo02.java
+++ test_thread/src/main/java/test/reflection/ReflectionDemo02.java
@@ -1,12 +1,22 @@
 package test.reflection;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class ReflectionDemo02 {
     static {
         System.out.println("进入main函数");
     }
 
     public static void main(String[] args) {
-
+        Date date = new Date();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.DAY_OF_YEAR,-2);
+        date = calendar.getTime();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        System.out.println(sdf.format(date));
     }
 }
 /*
